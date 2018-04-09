305e9aa14dcf79cdd77b5e2792135dbdae1f9fcf
STdev037 <gevorgmushyan>
2018-04-09 18:06:08 +0400

    login Wb elements are located

--- src/main/java/pageObject/accountPortal/login/LoginPageObject.java
+++ src/main/java/pageObject/accountPortal/login/LoginPageObject.java
@@ -12,9 +12,26 @@ import java.util.List;
 import static org.testng.Assert.assertEquals;
 
 public class LoginPageObject extends PageObject {
+    @FindBy(partialLinkText ="Petplan")
+    private WebElement logo;
+    @FindBy(name = "email")
+    private WebElement emailAddress;
+    @FindBy(name = "password")
+    private WebElement password;
+    @FindBy(id = "remember")
+    private WebElement rememberMe;
+    @FindBy(id = "imbLogin")
+    private WebElement signIn;
+    @FindBy(linkText = "forgot password?")
+    private WebElement forgotPassword;
+    @FindBy(partialLinkText = "start a quote")
+    private WebElement startAQuote;
 
     public LoginPageObject(WebDriver driver){
         super(driver);
+    }
+    public void ClickOnLogin(){
+        logo.click();
     }
 }
 
